16cb36fe86a3070b4124a1e2ecc58c0d954f1db8
Juan Manuel Torrejón <421483@tecnicatura.frc.utn.edu.ar>
2025-11-14 11:57:47 -0300

    Error en consola corregido

--- js/navigation.js
+++ js/navigation.js
@@ -952,15 +952,15 @@ function setupHomeButtonsVisibility() {
     }
 }
 
-// window.minions = function() {
-//     Swal.fire({
-//         title: '¡BANANA!',
-//         html: '<img src="https://media.giphy.com/media/hjvBoWTABwwNi/giphy.gif" alt="Minion" style="width: 100%;">',
-//         width: 600,
-//         padding: '1em',
-//         background: '#1e1d2c', // Tu color de fondo
-//         color: '#ffffff',      // Tu color de texto
-//         showConfirmButton: false, // Ocultamos el botón "OK"
-//         showCloseButton: true     // Mostramos una 'X' para cerrar
-//     });
-// }
+window.minions = function() {
+    Swal.fire({
+        title: '¡BANANA!',
+        html: '<img src="https://media.giphy.com/media/hjvBoWTABwwNi/giphy.gif" alt="Minion" style="width: 100%;">',
+        width: 600,
+        padding: '1em',
+        background: '#1e1d2c', // Tu color de fondo
+        color: '#ffffff',      // Tu color de texto
+        showConfirmButton: false, // Ocultamos el botón "OK"
+        showCloseButton: true     // Mostramos una 'X' para cerrar
+    });
+}
